66834b516dcc6751005237ffd1612d12a80e391f
Ellen <ellen31725@gmail.com>
2023-03-07 16:57:18 +0800

    feat: add dashboard link in restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -35,15 +35,27 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, // 拿出關聯的 Category model
-      nest: true,
-      raw: true
+      include: Category // 拿出關聯的 Category model
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('view_counts')
+      })
+      .then(restaurant => {
         res.render('restaurant', {
-          restaurant
+          restaurant: restaurant.toJSON()
         })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        res.render('dashboard', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
   }
